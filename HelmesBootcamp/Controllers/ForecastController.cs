52f307fcc97cf739c8303bc55c0ae7fcd5f32945
Vasiliy <Vasiliy@DARKSOULLOCK-54>
2017-03-26 14:38:01 +0300

    Added weather update

--- HelmesBootcamp/Controllers/ForecastController.cs
+++ HelmesBootcamp/Controllers/ForecastController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using HelmesBootcamp.Models;
@@ -34,9 +36,31 @@ namespace HelmesBootcamp.Controllers
             return b;
         }
 
+        private void UpdateForecast()
+        {
+            string url = "http://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php?lang=eng";
+            var wc = new WebClient();
+            var xml = wc.DownloadString(url);
+
+            var days = weatherRepository.FindAllAsQueryable(i => i.Date > DateTime.Now).ToList();
+
+            // probably not the best way.. probably can be replaced with xml deserialisation or linqToXml or something...
+            var day1rgx = new Regex(@"Harku[\s\S]+?phenomenon>(.*?)<");
+            var day1 = day1rgx.Matches(xml);
+            days[0].Snow = day1[0].Groups[1].Value.Contains("snow");
+
+            var day14rgx = new Regex(@"<day>[\s\S]+?phenomenon>(.*?)<");
+            var day14 = day14rgx.Matches(xml);
+            days[1].Snow = day14[1].Groups[1].Value.Contains("snow");
+            days[2].Snow = day14[2].Groups[1].Value.Contains("snow");
+            days[3].Snow = day14[3].Groups[1].Value.Contains("snow");
+        }
+
         public ActionResult Index()
         {
-            AddNewDays();
+            bool b = AddNewDays();
+            if (b)
+                UpdateForecast();
             return View(weatherRepository.FindAllAsQueryable(i=>i.Date>DateTime.Now));
         }
 
